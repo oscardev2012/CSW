d89af33867652882442e80c2b8771e41da7ccde1
oscardev2012 <oscar_ivan_sierra@hotmail.com>
2017-01-13 18:54:52 -0800

    Add files via upload

--- BackendAPI/App_Start/WebApiConfig.cs
+++ BackendAPI/App_Start/WebApiConfig.cs
@@ -23,6 +23,12 @@ namespace BackendAPI
             // To disable tracing in your application, please comment out or remove the following line of code
             // For more information, refer to: http://www.asp.net/web-api
             config.EnableSystemDiagnosticsTracing();
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+           
         }
     }
 }
